a73b7c4f44f568b62150563dc1040d343e97d7fb
Ow1Max <0w1.go.0w1@gmail.com>
2023-05-06 23:39:24 +0300

    refactor: Change text output with readable stream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-const path = require('path')
-const file = 'text.txt';
+const path = require('path');
+const file = `text.txt`;
+const finalPath = path.join(__dirname, file);
 
-fs.readFile(
-  path.join(__dirname, file),
-  'utf-8',
-  (err, data) => {
-    if (err) return console.error(err.message);
-    console.log(data);
-  }
-);
+const readableStream = fs.createReadStream(finalPath, 'utf-8');
+
+let data = '';
+
+readableStream.on('data', chunk => data += chunk);
+readableStream.on('end', () => console.log(data));
+readableStream.on('error', err => console.log(err.message));
