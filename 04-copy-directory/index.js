7c338d2f0e527bc07b2c03236f34c7f98163c720
Ow1Max <0w1.go.0w1@gmail.com>
2023-05-09 00:22:26 +0300

    feat: task '04-copy-directory' complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
+const originalFolder = 'files';
+const copiedFolder = 'files-copy';
+const originalPath = path.join(__dirname, originalFolder);
+const copiedPath = path.join(__dirname, copiedFolder);
 
+const copyDir = async () => {
+  try {
+    await fsPromises.mkdir(copiedPath, {recursive: true});
+
+    const originalFiles = await fsPromises.readdir(originalPath, { withFileTypes: true });
+    const copiedFiles = await fsPromises.readdir(copiedPath, { withFileTypes: true });
+
+    copiedFiles.forEach(async file => {
+      const copiedFile = path.join(copiedPath, file.name);
+
+      await fsPromises.unlink(copiedFile);
+    });
+
+    originalFiles.forEach(async file => {
+      if (file.isFile()) {
+        const originalFile = path.join(originalPath, file.name);
+        const copiedFile = path.join(copiedPath, file.name);
+
+        await fsPromises.copyFile(originalFile, copiedFile);
+      }
+    });
+  }
+  catch (err) {
+    console.log(err);
+  }
+};
+
+copyDir();
